7b3433c8960d8bcc67be825e32ef825eb9a00594
iswdp <iswdp@hotmail.com>
2018-10-27 11:07:29 -0700

    added index scraper

--- scraper/insider_scrape/get_prices.sh
+++ scraper/insider_scrape/get_prices.sh
@@ -4,7 +4,9 @@ cd ~/insider
 source venv/bin/activate
 cd scraper/insider_scrape/
 ./price_scrape.py
+./index_scrape.py
 deactivate
 cd ~/insider-observer/
 pipenv run ./manage.py shell < ./single_price_parse.py
+pipenv run ./manage.py shell < ./index_parse.py
 done
